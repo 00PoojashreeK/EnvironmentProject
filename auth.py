49229537760ed973bd71a642fc9e37a049299932
Poojashree K <nagashree184@gmail.com>
2026-05-13 22:59:44 +0530

    commit changes

--- auth.py
+++ auth.py
@@ -1,20 +1,33 @@
 import pandas as pd
 
 def signup(u, p, n, a, e):
+
     df = pd.read_csv("users.csv")
 
     if u in df["username"].values:
         return False
 
-    a = int(a)   # convert age to integer
+    a = int(a)
 
-    new = pd.DataFrame([[u, p, n, a, e, True]], columns=df.columns)
+    # FIXED
+    new = pd.DataFrame(
+        [[u, p, n, a, e]],
+        columns=df.columns
+    )
 
-    pd.concat([df, new]).to_csv("users.csv", index=False)
+    pd.concat([df, new], ignore_index=True).to_csv(
+        "users.csv",
+        index=False
+    )
 
     return True
 
 
 def login(u, p):
+
     df = pd.read_csv("users.csv")
-    return df[(df.username == u) & (df.password == p)]
+
+    return df[
+        (df.username == u) &
+        (df.password == p)
+    ]
